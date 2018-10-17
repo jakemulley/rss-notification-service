ad5f4ec956df3fff66257ee8a778c879d64b1834
Jake Mulley <mulleyjb@parliament.uk>
2018-10-17 15:42:51 +0100

    stuff

--- dynamodb/dynamodb.js
+++ dynamodb/dynamodb.js
@@ -10,34 +10,30 @@ let params = { TableName: 'topics' };
 
 const dynamodb = {
   populate(feeds) {
-    const totalCalls = Math.ceil(feeds.length / maxChunks),
-          params = { RequestItems: { topics: [] } },
-          promises = [];
+    const params = { RequestItems: { topics: [] } };
+    const promises = [];
+
+    feeds = feeds.filter(item => item.rss_link).map(value => {
+      const obj = {
+        PutRequest: {
+          Item: {
+            topic_id: { S: value.rss_link }
+          }
+        }
+      };
+
+      for(const key in value) {
+        obj.PutRequest.Item[key] = (value[key] ? { S: value[key].toString() } : { NULL: true });
+      }
+
+      return obj;
+    });
+
+    const totalCalls = Math.ceil(feeds.length / maxChunks);
 
     for (let i = 0; i < totalCalls; i++) {
       const chunk = params;
-      chunk.RequestItems.topics = feeds.slice(i * maxChunks, (i * maxChunks) + maxChunks).map((value) => {
-        if(value.rss_link) {
-          const obj = {
-            PutRequest: {
-              Item: {
-                topic_id: { S: value.rss_link }
-              }
-            }
-          };
-
-          for(const key in value) {
-            let val = { NULL: true };
-            if(value[key]) {
-              val = { S: value[key].toString() };
-            }
-            obj.PutRequest.Item[key] = val;
-          }
-
-          return obj;
-        }
-      });
-
+      chunk.RequestItems.topics = feeds.slice(i * maxChunks, (i * maxChunks) + maxChunks);
       promises.push(ddb.batchWriteItem(chunk).promise());
     }
 
--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const dynamodb = require('./dynamodb/dynamodb.js');
 const feeds = require('./mock/feeds.json');
 
-dynamodb.populate(feeds).then(function(result) {
-  console.log('done');
-}).catch(function(error) {
-  console.log('e', error);
-});
+dynamodb.populate(feeds);
